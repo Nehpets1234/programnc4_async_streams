ef5fd24c388dee5f23183b6971930a4ee56780b1
jaysonpardilla <jaysonpardilla2004@gmail.com>
2025-08-12 22:11:29 +0800

    Add Part 2: Error handling in Future

---
+++ programnc4_async_streams/lib/part2_error_handling.dart
@@ -0,0 +1,19 @@
+// part2_error_handling.dart
+
+// Function that simulates an error after 2 seconds
+Future<String> simulateError() {
+  return Future.delayed(Duration(seconds: 2), () {
+    throw Exception('Something went wrong!');
+  });
+}
+
+Future<void> main() async {
+  print('Starting error simulation...');
+
+  try {
+    String result = await simulateError();
+    print(result); // This won't execute because an error will be thrown
+  } catch (e) {
+    print('Caught an error: $e');
+  }
+}
